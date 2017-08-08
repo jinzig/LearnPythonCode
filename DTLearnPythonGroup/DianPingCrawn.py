40023b2d43eacb3d6dc4cdb0716497c4b051c679
jinzig <jinzig@gmail.com>
2017-08-08 23:41:25 +0800

    update 20170808

--- DTLearnPythonGroup/DianPingCrawn.py
+++ DTLearnPythonGroup/DianPingCrawn.py
@@ -8,15 +8,40 @@ import random
 import select
 
 
-def web_craw(url):
-    driver = webdriver.PhantomJS(executable_path='/Users/sallyfan/downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
-    driver.get(url)
-    comments = driver.find_element_by_xpath("//div[2]/div[2]/div/text()[2]")
-    comment = comments.xpath('string(.)').extract()[0]
-    for i in comments:
-        type(i)
-    type(comment)
-        #print(i)
+# def commnet_page_no(shopID):
+#     driver = webdriver.Safari()
+#     url = "http://www.dianping.com/shop/%s/review_more?pageno=1" % shopID
+#     driver.get(url)
+#
+#
+#     comments = driver.find_element_by_class_name("PageSel")
+#     for i in comments:
+#         i.text
+#     print(type(comment))
+#     print (i)
+#     commpg = comments
 
 
-web_craw("http://www.dianping.com/shop/4667684/review_more?pageno=59")
+def comments_wods(shopID, commpg):
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+    driver = webdriver.Chrome(chrome_options=options)
+    pg = 1
+    while pg <= commpg:
+        driver = webdriver.Chrome(executable_path='/Users/sallyfan/downloads/chromedriver')
+
+        # driver = webdriver.Safari()
+        url = "http://www.dianping.com/shop/%d/review_more?pageno=%d" % (shopID, pg)
+        driver.get(url)
+        pg +=1
+
+
+        comments_content = driver.find_elements_by_class_name("J_brief-cont")
+        for i in comments_content:
+            i.text
+            all_comments.append()
+        else:
+            print('over哦')
+    print (all_comments)
+
+print(comments_wods(90455659, 32))
