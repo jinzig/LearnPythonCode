9b7d05a670a479daffb81a8479b2ac9f5c5e7969
jinzig <jinzig@gmail.com>
2017-08-17 23:39:15 +0800

    update 20170808

--- DTLearnPythonGroup/DianPingCrawn.py
+++ DTLearnPythonGroup/DianPingCrawn.py
@@ -6,53 +6,77 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import random
 import select
+import PIL
 
 
-
-
+# shopID = int(input("input shopid please:  "))
+# commpg = int(input("page number of shop:  "))
 
 def comments_wods(shopID, commpg):
     uaList = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586',
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0']
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0',
+        "Mozilla/5.0 (BeOS; U; BeOS BeBox; fr; rv:1.9) Gecko/2008052906 BonEcho/2.0",
+"Mozilla/5.0 (BeOS; U; BeOS BePC; en-US; rv:1.8.1.1) Gecko/20061220 BonEcho/2.0.0.1",
+'Mozilla/5.0 (BeOS; U; BeOS BePC; en-US; rv:1.8.1.10) Gecko/20071128 BonEcho/2.0.0.10',
+'Mozilla/5.0 (BeOS; U; BeOS BePC; en-US; rv:1.8.1.17) Gecko/20080831 BonEcho/2.0.0.17',
+'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+        'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)',
+        'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+        'Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)'
+    ]
 
     dcap = dict(webdriver.DesiredCapabilities.PHANTOMJS)
 
     dcap["phantomjs.page.settings.userAgent"] = (random.choice(uaList))
 
-    pg = 1
-    while pg <= commpg:
-        driver = webdriver.Chrome(executable_path='/Users/sallyfan/downloads/chromedriver')
-        # driver = webdriver.PhantomJS(executable_path='/Users/sallyfan/downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
-
+    pgno = 1
+    while pgno <= commpg:
+        # driver = webdriver.Chrome(executable_path='/Users/sallyfan/downloads/chromedriver')
+        driver = webdriver.PhantomJS(executable_path='/Users/sallyfan/downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
         # driver = webdriver.Safari()
-        url = "http://www.dianping.com/shop/%d/review_more?pageno=%d" % (shopID, pg)
+        url = "http://www.dianping.com/shop/%d/review_more?pageno=%d" % (shopID, pgno)
         driver.get(url)
-        pg +=1
+        pgno +=1
         all_comments = []
 
 
         comments_content = driver.find_elements_by_class_name("J_brief-cont")
-        with open('dianping.txt', 'w+') as f:
+        with open('dianping.txt', 'r+') as f:
             for i in comments_content:
                 all_comments.append(i.text)
-            pg_comment= "".join(all_comments)
+        pg_comment= "".join(all_comments)
+        # f.write(pg_comment)
+
                 # print (i.text)
-            print(pg_comment)
+        print(pg_comment)
+
     return pg_comment
+
             # f.write()
 
+
+
 import jieba
-wordlist = jieba.cut(comments_wods(), cut_all = True)
+wordlist = jieba.cut(comments_wods(16795494,3), cut_all = True)
 word_space_split = " ".join(wordlist)
+
+# print(comments_wods(16795494, 10))
 
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, ImageColorGenerator
 import numpy as np
-import Pil.Image as Image
-coloring = np.array(Image.open("/Users/apple/......."))
-my_wordcloud = WordCloud(background_color = "white", max_words = 5000, mask = coloring, max_font_size = 60, random_state = 42, scale = 2, font_path = "....").generate(word_space_split)
+import PIL.Image as Image
+coloring = np.array(Image.open("/Users/sallyfan/downloads/mi.jpg"))
+my_wordcloud = WordCloud(background_color = "white", max_words = 50000, mask = coloring, max_font_size = 30, random_state = 42, scale = 2, font_path = "/Users/sallyfan/downloads/msyh.ttf").generate(word_space_split)
 image_colors = ImageColorGenerator(coloring)
 plt.imshow(my_wordcloud.recolor(color_func=image_colors))
 plt.imshow(my_wordcloud)
@@ -62,4 +86,3 @@ plt.show()
 
     # print (comments_content)
 
-print(comments_wods(16795494, 72))
